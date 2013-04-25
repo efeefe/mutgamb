4051d9ff407867b44d15a213bcc907199613c26d
Felipe Fonseca <felipefonseca@gmail.com>
2013-04-25 17:30:43 +0200

    publicando o acervo mutgamb

---
+++ mutsaz/2010/pozimi/src/montando_lyx_outro/parciais/finalizando_mutsaz_pozimi_ebooks/images.pl
@@ -0,0 +1,102 @@
+# LaTeX2HTML 2008 (1.71)
+# Associate images original text with physical files.
+
+
+$key = q/includegraphics[scale=0.7]{imagensslashr4re_pozimi3p_ebook};LFS=12;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="358" HEIGHT="238" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img6.png"
+ ALT="\includegraphics[scale=0.7]{imagens/r4re_pozimi3p_ebook}">|; 
+
+$key = q/includegraphics[scale=0.7]{imagensslashr4re_pozimi4p_ebook};LFS=12;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="358" HEIGHT="238" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img8.png"
+ ALT="\includegraphics[scale=0.7]{imagens/r4re_pozimi4p_ebook}">|; 
+
+$key = q/includegraphics[scale=0.7]{imagensslashorlando_1__ebook};LFS=12;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="358" HEIGHT="268" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img9.png"
+ ALT="\includegraphics[scale=0.7]{imagens/orlando_1__ebook}">|; 
+
+$key = q/includegraphics[scale=0.7]{imagensslashr4re_pozimi5p_ebook};LFS=12;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="358" HEIGHT="238" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img7.png"
+ ALT="\includegraphics[scale=0.7]{imagens/r4re_pozimi5p_ebook}">|; 
+
+$key = q/includegraphics{imagensslashcapainverno_limpa_ebook};LFS=12;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="511" HEIGHT="666" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img1.png"
+ ALT="\includegraphics{imagens/capainverno_limpa_ebook}">|; 
+
+$key = q/includegraphics[scale=0.7]{imagensslashorlando_4_4942002996_1765ffcdc53_ebook};LFS=12;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="358" HEIGHT="201" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img12.png"
+ ALT="\includegraphics[scale=0.7]{imagens/orlando_4_4942002996_1765ffcdc53_ebook}">|; 
+
+$key = q/includegraphics[scale=0.7]{imagensslashorlando_6_Viaj_ebook};LFS=12;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="358" HEIGHT="268" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img14.png"
+ ALT="\includegraphics[scale=0.7]{imagens/orlando_6_Viaj_ebook}">|; 
+
+$key = q/includegraphics[scale=0.7]{imagensslashorlando_3_4942002996_1765ffcdc54_ebook};LFS=12;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="358" HEIGHT="268" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img11.png"
+ ALT="\includegraphics[scale=0.7]{imagens/orlando_3_4942002996_1765ffcdc54_ebook}">|; 
+
+$key = q/includegraphics[scale=0.7]{imagensslashorlando_2_4942002996_1765ffcdc52_ebook};LFS=12;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="358" HEIGHT="201" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img10.png"
+ ALT="\includegraphics[scale=0.7]{imagens/orlando_2_4942002996_1765ffcdc52_ebook}">|; 
+
+$key = q/includegraphics[scale=0.7]{imagensslashsilia_ebook};LFS=12;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="358" HEIGHT="268" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img3.png"
+ ALT="\includegraphics[scale=0.7]{imagens/silia_ebook}">|; 
+
+$key = q/includegraphics{imagensslashmutirantes_limpo_ebook};LFS=12;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="511" HEIGHT="662" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img2.png"
+ ALT="\includegraphics{imagens/mutirantes_limpo_ebook}">|; 
+
+$key = q/includegraphics{imagensslashpozimi_contracapa_ebook};LFS=12;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="368" HEIGHT="481" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img16.png"
+ ALT="\includegraphics{imagens/pozimi_contracapa_ebook}">|; 
+
+$key = q/includegraphics[scale=0.7]{imagensslashyzak_ebook};LFS=12;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="358" HEIGHT="171" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img15.png"
+ ALT="\includegraphics[scale=0.7]{imagens/yzak_ebook}">|; 
+
+$key = q/includegraphics[scale=0.7]{imagensslashr4re_pozimi2p_ebook};LFS=12;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="358" HEIGHT="238" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img5.png"
+ ALT="\includegraphics[scale=0.7]{imagens/r4re_pozimi2p_ebook}">|; 
+
+$key = q/includegraphics[scale=0.7]{imagensslashorlando_5_4942006698_b200f1c6fd_ebook};LFS=12;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="358" HEIGHT="201" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img13.png"
+ ALT="\includegraphics[scale=0.7]{imagens/orlando_5_4942006698_b200f1c6fd_ebook}">|; 
+
+$key = q/includegraphics[scale=0.7]{imagensslashr4re_pozimi1p_ebook};LFS=12;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="358" HEIGHT="238" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img4.png"
+ ALT="\includegraphics[scale=0.7]{imagens/r4re_pozimi1p_ebook}">|; 
+
+1;
+
